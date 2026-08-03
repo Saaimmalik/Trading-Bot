1f1f7c893a5f3d90afac56c87513162fc4544d3c
Saaim Malik <saaimmalik832@gmail.com>
2026-08-04 00:55:15 +0500

    built limit order request functionality

--- alpaca_test.py
+++ alpaca_test.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 from alpaca.trading.client import TradingClient
-from alpaca.trading.requests import MarketOrderRequest
+from alpaca.trading.requests import LimitOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
 
 
@@ -12,12 +12,13 @@ API_SECRET = os.getenv("APCA_API_SECRET")
 
 trading_client = TradingClient(API_KEY, API_SECRET)
 
-market_order_data = MarketOrderRequest(
+limit_order_data = LimitOrderRequest(
     symbol="SPY",
     qty=1,
     side=OrderSide.BUY,
-    time_in_force=TimeInForce.DAY
+    time_in_force=TimeInForce.DAY,
+    limit_price=757.00
 )
 
-market_order = trading_client.submit_order(market_order_data)
-print(market_order)
+limit_order = trading_client.submit_order(limit_order_data)
+print(limit_order)
