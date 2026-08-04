77ef2a62e159f64b080c06427b2f95a9de4514d1
Saaim Malik <saaimmalik832@gmail.com>
2026-08-04 15:29:57 +0500

    create and show positions

--- alpaca_test.py
+++ alpaca_test.py
@@ -1,9 +1,6 @@
 from dotenv import load_dotenv
 import os
 from alpaca.trading.client import TradingClient
-from alpaca.trading.requests import LimitOrderRequest
-from alpaca.trading.enums import OrderSide, TimeInForce
-
 
 load_dotenv()
 
@@ -12,13 +9,7 @@ API_SECRET = os.getenv("APCA_API_SECRET")
 
 trading_client = TradingClient(API_KEY, API_SECRET)
 
-limit_order_data = LimitOrderRequest(
-    symbol="SPY",
-    qty=1,
-    side=OrderSide.BUY,
-    time_in_force=TimeInForce.DAY,
-    limit_price=757.00
-)
+positions = trading_client.get_all_positions()
 
-limit_order = trading_client.submit_order(limit_order_data)
-print(limit_order)
+for position in positions:
+    print(position.symbol, position.current_price)
